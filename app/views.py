16fa307514f200b1ec638a0a95bd19b7c220db7e
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-02-16 20:08:38 +0530

    product details added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-
+from django.db.models import Count
 from django.shortcuts import render
+from django.views import View
+from . models import Product
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product=Product.objects.filter(category=val)
+        title=Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        return render(request,"app/productdetail.html",locals())
