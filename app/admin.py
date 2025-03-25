928db6c10d288da4dfe796b0acb8fce40dd65692
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-03-25 20:27:29 +0530

    modelPage_Change

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from . models import Customer,Product,Cart
-
-
+from . models import Customer, Payment,Product,Cart,OrderPlaced
 
 # Register your models here.
 
@@ -9,12 +7,18 @@ from . models import Customer,Product,Cart
 class ProductModelAdmin(admin.ModelAdmin):
     list_display=['id','title','discounted_price','category','product_image']
 
-
 @admin.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
     list_display=['id','user','locality','city','state','zipcode']
     
-    
 @admin.register(Cart)
 class cartModelAdmin(admin.ModelAdmin):
     list_display=['id','user','product','quantity']
+    
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+    
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','quantity','status','payment']
